87a742f5255a0199714e65297d59adf77594ed8f
Fuu <98339035+notyourfriends@users.noreply.github.com>
2022-05-06 22:11:11 +0700

    Equals from Add Function -16
    Not complete yet but can run

--- calculator-15.py
+++ calculator-15.py
@@ -14,6 +14,105 @@ class MyLayout(Widget):
 	def clear(self):
 		self.ids.calc_input.text= '0'
 
+	#Create Press Button func
+	def button_press(self, button):
+		# variabel penampung
+		prior = self.ids.calc_input.text
+
+		# determine if 0 is sitting there
+		if prior == "0":
+			self.ids.calc_input.text = " "
+			self.ids.calc_input.text = f'{button}'
+		else:
+			self.ids.calc_input.text = f'{prior}{button}'
+
+	# penjumlahan
+	def add(self):
+		# variable penampung
+		prior = self.ids.calc_input.text
+
+		# put + sign
+		self.ids.calc_input.text = f'{prior}+'
+
+	#pengurangan
+	def subtract(self):
+		# variable penampung
+		prior = self.ids.calc_input.text
+
+		# put + sign
+		self.ids.calc_input.text = f'{prior}-'
+
+	#pembagian
+	def divide(self):
+		# variable penampung
+		prior = self.ids.calc_input.text
+
+		# put + sign
+		self.ids.calc_input.text = f'{prior}/'
+
+	#perkalian
+	def multiply(self):
+		# variable penampung
+		prior = self.ids.calc_input.text
+
+		# put + sign
+		self.ids.calc_input.text = f'{prior}*'
+
+	#hasil
+	def equals(self):
+		# variable penampung
+		prior = self.ids.calc_input.text
+
+		#Addition
+		if "+" in prior:
+			num_list = prior.split("+")
+			answer = 0
+
+			#loop thru out list
+			for number in num_list:
+				answer = answer + int(number)
+			
+			#print the answer in the text box
+			self.ids.calc_input.text = str(answer)
+
+		#Subtract
+		if "-" in prior:
+			num_list = prior.split("-")
+			answer = 0
+
+			#loop thru out list
+			for number in num_list:
+				answer = answer - int(number)
+			
+			#print the answer in the text box
+			self.ids.calc_input.text = str(answer)
+
+		#Multiply
+		if "*" in prior:
+			num_list = prior.split("*")
+			answer = 0
+
+			#loop thru out list
+			for number in num_list:
+				answer = answer * int(number)
+			
+			#print the answer in the text box
+			self.ids.calc_input.text = str(answer)
+
+		#Divide
+		if "/" in prior:
+			num_list = prior.split("/")
+			answer = 0
+
+			#loop thru out list
+			for number in num_list:
+				answer = answer / int(number)
+			
+			#print the answer in the text box
+			self.ids.calc_input.text = str(answer)
+
+
+
 class CalculatorApp(App):
 	def build(self):
 		return MyLayout()
