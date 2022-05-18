d3e553b797bce4c8b42f5289a8a82db69534b83e
Fuu <98339035+notyourfriends@users.noreply.github.com>
2022-05-18 20:19:16 +0700

    Fixing images button

--- season2/images_button.py
+++ season2/images_button.py
@@ -6,7 +6,14 @@ from kivy.uix.widget import Widget
 Builder.load_file('images_button.kv')
 
 class MyLayout(Widget):
-	pass
+	def click_on(self):
+		self.ids.my_label.text = 'Press the image'
+		self.ids.my_image.source = 'images/after.png'
+
+	def click_off(self):
+		#self.ids.my_label.text = 'now long press the image'
+		self.ids.my_image.source = 'images/before.png'
+
 
 class AwesomeApp(App):
 	def build(self):
